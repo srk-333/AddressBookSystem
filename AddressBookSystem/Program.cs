6953d764bfad3aa35e16d68eadad85367749359d
srk-333 <sauravmk333@gmail.com>
2022-01-19 22:36:53 +0530

    [Saurav]Add: Welcome Message

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Program
+    {
+        //Main  Method / program Entry Point
+        static void Main(string[] args)
+        {
+            //Showing Welcome Message On console.
+            Console.WriteLine("Welcome to Address Book Progam");
+            Console.ReadLine();
+        }
+    }
+}
