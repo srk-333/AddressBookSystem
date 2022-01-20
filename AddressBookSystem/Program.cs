0a2aa933f1da7e8a87076ddb7d4fd888c3a77e44
srk-333 <sauravmk333@gmail.com>
2022-01-20 22:20:21 +0530

    [Saurav]Add:Created a contact in address Book

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -12,7 +12,21 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             //Showing Welcome Message On console.
-            Console.WriteLine("Welcome to Address Book Progam");
+            Console.WriteLine("Welcome to Address Book Progam \n");
+
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            CreateAddressBook addressBook = new CreateAddressBook()  //Initializing elements using collection-initializer syntax
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            addressBook.displayContact();
             Console.ReadLine();
         }
     }
